f298dd201613e7e87a27e39ab261c4a9577cb5d3
Steve Regester <steve@studylog.com>
2022-12-22 23:43:54 -0800

    Follow the dox

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
-const db = require('@cyclic.sh/dynamodb')
 const get = require('lodash/get')
 const path = require('path')
-const Chance = require('chance')
 const express = require('express')
 const bodyParser = require('body-parser')
 
+const Chance = require('chance')
+const CyclicDb = require('@cyclic.sh/dynamodb')
+
+const db = new CyclicDb('shortcodes')
 const app = express()
 const lootDir = path.resolve(__dirname, '..', 'loot')
 const allowedStatuses = [ 300, 301, 302, 303, 304, 307 ]
