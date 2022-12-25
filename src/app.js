b45fa86ec38e58f39338b9c545e982b852332bd6
Steve Regester <steve@studylog.com>
2022-12-25 11:37:46 -0800

    Add tracking

--- src/app.js
+++ src/app.js
@@ -11,8 +11,10 @@ const Chance = require('chance')
 
 const app = express()
 const shortcodes = db.collection('shortcodes')
-const allowedStatuses = [ 300, 301, 302, 303, 304, 307 ]
+const shortcodeCreations = db.collection('shortcode_creations')
+const shortcodeInvocations = db.collection('shortcode_invocations')
 
+const allowedStatuses = [ 300, 301, 302, 303, 304, 307 ]
 const appPort = get(process, 'env.PORT', 3000)
 const appWebsite = get(process, 'env.WEBSITE', 'https://localhost/')
 const appProtocol = get(process, 'env.PROTOCOL', 'https')
@@ -75,34 +77,63 @@ app.post('/_new',
     }
 
     const shortcode = await generateShortcode()
+    const { protocol, ip, method, baseUrl, path, params, query, body } = req
+
     await shortcodes.set(shortcode, { redirect, status })
-    const record = await shortcodes.get(shortcode)
-    res.json(formatShortcodeRecord(record))
+    await shortcodeCreations.set(shortcode, { protocol, ip, method, baseUrl, path, params, query, body })
+
+    const shortcodeRecord = await shortcodes.get(shortcode)
+    res.json(formatShortcodeRecord(shortcodeRecord))
   })
 
 // READ
 app.get('/_list', authenticationMiddleware, async (req, res) => {
   const { results } = await shortcodes.list(Infinity)
-  const records = await Promise.all(results.map(({ key }) => shortcodes.get(key)))
-  res.json(records.map(formatShortcodeRecord))
+  const shortcodeRecords = await Promise.all(results.map(({ key }) => shortcodes.get(key)))
+  res.json(shortcodeRecords.map(formatShortcodeRecord))
+})
+
+app.get('/:shortcode/_metadata', authenticationMiddleware, async (req, res) => {
+  try {
+    const shortcode = get(req, 'params.shortcode')
+    const shortcodeRecord = await shortcodes.get(shortcode)
+    if (!shortcodeRecord) return res.sendStatus(404)
+
+    const record = formatShortcodeRecord(shortcodeRecord)
+    const { props: creation } = await shortcodeCreations.get(shortcode)
+    const { results: invocationRecords } = await shortcodeInvocations.filter({ shortcode })
+    res.json({ record, creation, invocations: invocationRecords.map(({ props }) => props) })
+  } catch (e) {
+    res.sendStatus(500, e.message)
+  }
 })
 
 // DELETE
 app.delete('/:shortcode', authenticationMiddleware, async (req, res) => {
-  const shortcode = get(req, 'params.shortcode')
-  const record = await shortcodes.get(shortcode)
-  if (!record) return res.sendStatus(404)
-  try { await shortcodes.delete(shortcode) }
-  catch (e) { res.sendStatus(500, e.message) }
-  return res.sendStatus(200)
+  try {
+    const shortcode = get(req, 'params.shortcode')
+    const shortcodeRecord = await shortcodes.get(shortcode)
+    if (!shortcodeRecord) return res.sendStatus(404)
+
+    await shortcodes.delete(shortcode)
+    res.sendStatus(200)
+  } catch (e) {
+    res.sendStatus(500, e.message)
+  }
 })
 
 // Redirect
 app.use('/:shortcode', async (req, res, next) => {
   try {
     const shortcode = get(req, 'params.shortcode')
-    const record = await shortcodes.get(shortcode)
-    const { status, redirect } = formatShortcodeRecord(record)
+    const shortcodeRecord = await shortcodes.get(shortcode)
+    if (!shortcodeRecord) return next()
+
+    const { status, redirect } = formatShortcodeRecord(shortcodeRecord)
+    const { protocol, ip, method, baseUrl, path, params, query, body } = req
+    const invocationKey = `${Date.now()} ${ip} ${method} ${shortcode}`
+    await shortcodeInvocations.set(invocationKey, { shortcode, protocol, ip, method, baseUrl, path, params, query, body })
+
     res.redirect(status, redirect)
   } catch (e) { return next() }
 })
