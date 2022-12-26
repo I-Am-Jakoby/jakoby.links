f04da6fa4f28a97524b2750cd0668e8c0f29ec99
Steve Regester <steve@studylog.com>
2022-12-25 20:31:36 -0800

    Add delete maintenance script with friendly alert for dicsord hooks

---
+++ scripts/delete_301_webhooks.js
@@ -0,0 +1,60 @@
+#!/usr/bin/env node
+
+const axios = require('axios')
+
+// const appRoot = 'https://good-puce-salmon-fez.cyclic.app'
+// const appPass = 'iamnotjakoby'
+
+const appRoot = 'https://link.iamjakoby.com'
+const appPass = 'Iamjakobylinks3000!'
+
+const authParams = {
+  headers: {
+    Authorization: `Bearer ${appPass}`
+  }
+}
+
+;(async () => {
+  const listResponse = await axios.get(`${appRoot}/_list`, authParams)
+
+  listResponse.data.forEach(({ status, redirect, shortcode }) => {
+    const looksLikeWebhook = /webhook/.test(redirect)
+    const isWebhookStatus = status === 307
+    if (!looksLikeWebhook || isWebhookStatus) return
+
+    const looksLikeDiscordWebhook = /^https:\/\/(?:canary\.)?discord\.com\/api\/webhooks/.test(redirect)
+    if (looksLikeDiscordWebhook) {
+      console.info('Shortcode', shortcode, 'looks like a discord webhook. Sending a friendly message.')
+
+      axios.post(redirect, {
+        username: 'Steve H. Ackerman#1438',
+        avatar_url: 'https://cdn.discordapp.com/avatars/805590585826082818/ca6ca3e427619a13fa3ee528d5d030d1.webp',
+        content: `
+It looks like you're trying to use \`${appRoot}\` to shorten a webhook.
+You should use status \`307\` instead of \`301\`.
+THIS SHORTCODE (\`${shortcode}\`) WILL BE DELETED.
+Check out this article for more information: https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/307
+        `,
+        embeds: [{
+          title: 'Shortcode',
+          fields: [{
+            name: 'Shortcode',
+            value: shortcode,
+            inline: true
+          }, {
+            name: 'Status',
+            value: status,
+            inline: true
+          }, {
+            name: 'Redirect',
+            value: redirect
+          }]
+        }]
+      })
+    }
+
+    console.info('DELETING:', shortcode, 'uses status', status, 'but looks like a webhook', redirect)
+    axios.delete(`${appRoot}/${shortcode}`, authParams)
+  })
+
+})()
